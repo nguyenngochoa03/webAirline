b9b7181832733b33ff3caf7801d81a3a125eaf01
nguyenngochoa03 <ngochoa032111@gmail.com>
2023-07-17 10:02:14 +0700

    ngay16/7

---
+++ WebAirline/app/Models/Hanhtrinhbay.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hanhtrinhbay extends Model
+{
+    use HasFactory;
+    protected $table = "hanhtrinhbay";
+}
---
+++ WebAirline/database/migrations/2023_07_16_155430_createh_hanhtrinhbay_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('hanhtrinhbay', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure');
+            $table->string('destination');
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('passenger')->nullable();
+            $table->string('seat_class')->nullable();
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('hanhtrinhbay');
+    }
+};
